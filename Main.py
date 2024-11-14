b4ea3b1790a964fd9b61e55901c2388923c7d624
avinaash27sept <avinaash27sept@gmail.com>
2024-11-14 18:16:48 +0530

    updaated logo and graph

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+LOGO_URL_LARGE="logo/logo.png"
+st.logo(LOGO_URL_LARGE,link="https://streamlit.io/gallery",size="large")
 
 st.title(":blue[Titanic Ship Survival Prediction System]:")
 pages = {
--- visualization.py
+++ visualization.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 import DataFrame_Description as data
 Heatmap, CountPlot, BoxPlot = st.tabs(['Heat Map','Count Plot','Box Plot'])
 with Heatmap:
-    st.title("Heat Map")    
-    st.image("graphs/heatmap.png") 
+    h = sns.heatmap(data.train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+    fig=h.get_figure()
+    st.write(fig)
