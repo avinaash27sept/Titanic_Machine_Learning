06c69c1f13401af8018ca4d4a1b9b2e26d4373af
Siddu31103 <siddhantgaikwad579@gmail.com>
2024-11-14 10:56:24 +0530

    Updated
    Design Update

--- Main.py
+++ Main.py
@@ -5,7 +5,15 @@ import numpy as np
 st.title(":blue[Titanic Ship Survival Prediction System]:")
 pages = {
 
-    "Data" : [st.Page("DataFrame_Description.py"),st.Page("visualization.py"),st.Page("analysis.py")]
+    "Data" : [st.Page("DataFrame_Description.py"),st.Page("visualization.py"),st.Page("analysis.py")],
+    "Data Cleaning" :[st.Page("Cleaned_DataFrame_Description.py"),st.Page("Data_Cleaning_Visualization.py")],
+    "Models" :[st.Page("Name_of_Model.py")],
+    "Result Comparison" :[st.Page("Result_table.py"),st.Page("Model_selection_and_justification.py")],
+    "Parameter Tuning" :[st.Page("Parameter_list_for_tuning.py"),st.Page("Result_after_parameter_tuning.py")],
+    "Test on Live Data" :[st.Page("Form_to_submit_data.py")]
+   
+  
+    
 }
 
 pg = st.navigation(pages)
