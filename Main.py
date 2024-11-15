c2d4a20a021af23a9ae3c83989d8b6736ccf0002
avinaash27sept <avinaash27sept@gmail.com>
2024-11-15 08:48:32 +0530

    logo updated

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 LOGO_URL_LARGE="resources/logo/logo.png"
-st.logo(LOGO_URL_LARGE,link="https://streamlit.io/gallery",size="large")
+st.logo(LOGO_URL_LARGE,size="large")
 train = pd.read_csv("resources/csv/titanic_train.csv")
 st.title(":blue[Titanic Ship Survival Prediction System]:")
 pages = {
@@ -14,8 +14,6 @@ pages = {
     "Parameter Tuning" :[st.Page("src/modelTuning/Parameter_list_for_tuning.py"),st.Page("src/modelTuning/Result_after_parameter_tuning.py")],
     "Test on Live Data" :[st.Page("src/Test/Form_to_submit_data.py")]
    
-  
-    
 }
 
 pg = st.navigation(pages)
